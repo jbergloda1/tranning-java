1ce256ce895541025bf14c63459a4f65eee67e81
Linh Tran Nhat <tnhatlinh@tma.com.vn>
2022-09-13 14:44:04 +0700

    config deploy

--- src/main/java/com/dc24/tranning/TranningApplication.java
+++ src/main/java/com/dc24/tranning/TranningApplication.java
@@ -2,14 +2,13 @@ package com.dc24.tranning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+
+@SpringBootApplication
 public class TranningApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TranningApplication.class, args);
 	}
 
 }
-
 
