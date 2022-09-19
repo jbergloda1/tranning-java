1b3294784ee73751d74e3053d39da7fe89a09d6d
Linh Tran Nhat <tnhatlinh@tma.com.vn>
2022-09-19 16:32:02 +0700

    implement login authentication

--- src/main/java/com/dc24/tranning/controller/UserController.java
+++ src/main/java/com/dc24/tranning/controller/UserController.java
@@ -20,13 +20,11 @@ import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 
+@CrossOrigin(origins = { "http://localhost:8080"})
 @RestController
 @RequestMapping("/api/v1/auth")
 public class UserController {
