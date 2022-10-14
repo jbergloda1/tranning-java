0e7e25ebea4f77ef706f1a21e85e70671f97d389
jbloda1 <jbloda1@gmail.com>
2022-10-14 13:28:09 +0700

    implement security and jwt

--- src/main/java/com/dc24/tranning/constant/MailConstant.java
+++ src/main/java/com/dc24/tranning/constant/MailConstant.java
@@ -2,7 +2,7 @@ package com.dc24.tranning.constant;
 
 public class MailConstant {
     public static final String MY_EMAIL = "jbloda1@gmail.com";
-    public static final String MY_PASSWORD = "Nl@16051997";
+    public static final String MY_PASSWORD = "qtgjeuytiyqgxzkw";
     public static final String Project_EMAIL = "JavaTranning@gmail.com";
 
     public static final String PORT = "587";
